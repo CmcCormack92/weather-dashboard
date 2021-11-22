19cca131f9ac1e2d5a4e0b62654d5468b9019153
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2021-11-21 20:29:34 -0500

    get coordinates of city entered into input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
-var geoUrl = "http://api.geonames.org/searchJSON?q=";
-var geoApiKey = "&username=cmccormack92";
+var cityInputEL = document.querySelector("#city-input");
+var citySearchEl = document.querySelector("#city-search")
+
+
+var inputSubmitHandler = function () {
+    event.preventDefault();
+
+    var city = cityInputEL.value.toLowerCase().trim();
+
+    if (city) {
+        getCoordinates(city);
+
+        cityInputEL.value = "";
+    } else {
+        alert("Please Enter a City Name")
+    }
+}
+
+var getCoordinates = function (city) {
+    var geoUrl = "http://api.geonames.org/searchJSON?q=" + city + "&username=cmccormack92";
+
+    fetch(geoUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                var longitude = data.geonames[0].lng;
+                var lattitude = data.geonames[0].lat;
+
+                console.log(longitude);
+                console.log(lattitude);
+            })
+        }
+    })
+};
+
+
+citySearchEl.addEventListener('submit', inputSubmitHandler)
+
+
+
+
+
+
 
