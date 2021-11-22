0d79ea842a14d443f5c63be1a5f2525f775f538e
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2021-11-21 21:23:23 -0500

    buttons for each recently viewed city add programatically and then save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
 var cityInputEL = document.querySelector("#city-input");
-var citySearchEl = document.querySelector("#city-search")
+var citySearchEl = document.querySelector("#city-search");
+var savedCitiesEl = document.querySelector("#saved-cities");
+var clearSaveEl = document.querySelector("#clear-btn");
+
+var savedCitiesArr = [];
 
 
 var inputSubmitHandler = function () {
     event.preventDefault();
 
-    var city = cityInputEL.value.toLowerCase().trim();
+    var city = cityInputEL.value.trim();
 
     if (city) {
         getCoordinates(city);
+        createSavedSearch(city);
 
         cityInputEL.value = "";
+        savedCitiesArr.push(city);
+        localStorage.setItem("saved-city", JSON.stringify(savedCitiesArr));
     } else {
         alert("Please Enter a City Name")
     }
@@ -25,16 +32,51 @@ var getCoordinates = function (city) {
                 var longitude = data.geonames[0].lng;
                 var lattitude = data.geonames[0].lat;
 
-                console.log(longitude);
-                console.log(lattitude);
+                // console.log(longitude);
+                // console.log(lattitude);
             })
         }
     })
 };
 
+var createSavedSearch = function (city) {
+    var savedCityBtn = document.createElement("button");
+    savedCityBtn.classList = "btn btn-lg btn-secondary col-12 text-light rounded my-1"
+    savedCityBtn.textContent = city;
+
+    savedCitiesEl.appendChild(savedCityBtn);
+};
+
+var getSavedCities = function () {
+    var storage = JSON.parse(localStorage.getItem("saved-city"));
+
+    if (storage === null) {
+        storage = [];
+    } else {
+        savedCitiesArr = storage;
+    }
+};
+
+var showSavedCities = function () {
+    getSavedCities();
+
+    for (var i = 0; i < savedCitiesArr.length; i++) {
+        var savedCityBtn = document.createElement("button");
+        savedCityBtn.classList = "btn btn-lg btn-secondary col-12 text-light rounded my-1"
+        savedCityBtn.textContent = savedCitiesArr[i];
+
+        savedCitiesEl.appendChild(savedCityBtn);
+    };
+};
+
+var clearLocal = function() {
+    localStorage.clear();
+    savedCitiesEl.remove("button");
+};
 
 citySearchEl.addEventListener('submit', inputSubmitHandler)
-
+clearSaveEl.addEventListener('click', clearLocal)
+showSavedCities()
 
 
 
